16bcf44c8f32f940c9e50bd38e74f221bd29ccbe
Netanel Mizrahi <nmiz1987@gmail.com>
2022-04-26 00:24:34 +0300

    add api

--- server.js
+++ server.js
@@ -21,22 +21,35 @@ app.get('/', (req, res) => {
   res.render('index')
 })
 
+app.get('/news', async (req, res) => {
+  const url = 'https://newsapi.org/v2/top-headlines?country=il&category=technology&apiKey=13d5500121e54393b43e2584e93a6f72'
+try{
+  const res = await fetch(url);
+  const obj = await res.json();
+  res.status(201).json(obj)
+}
+catch (err){
+  res.status(400).json( { message: err.message})
+}
+  res.render('index')
+})
+
 
 app.get('/todo', (req, res) => {
-  res.json({ "All ToDo": db })
+  res.json({ db })
 })
 
 app.get('/addTodo/:text', (req, res) => {
-  const newTodo = { complete: false, id: id++, text: req.params.text }
+  const newTodo = { complete: false, id: id++, text: req.params.text, Date: new Date() }
   db.push(newTodo)
-  res.json({ "All ToDo": db })
+  res.json({ newTodo })
 })
 
 app.post('/todo', (req, res) => {
-  const newTodo = { complete: false, id: id++, text: req.body.text }
+  const newTodo = { complete: false, id: id++, text: req.body.text, Date: new Date()  }
   db.push(newTodo)
 
-  res.json({ "New ToDo:": newTodo })
+  res.json({ newTodo })
 })
 
 app.listen(process.env.PORT || 8000, () => {
